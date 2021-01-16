f108523b5d470c236b5e1f476460be5616a7e889
Demozy4321 <56303334+Demozy4321@users.noreply.github.com>
2021-01-16 10:38:06 +0530

    update navbar
    added the classname for styling the navbar.

--- todo_list/src/components/NavBar.js
+++ todo_list/src/components/NavBar.js
@@ -10,7 +10,7 @@ import GoogleAuth from './GoogleAuth';
 const NavBar = () => {
   return (
     <div>
-      <Navbar color="light" light expand="md">
+      <Navbar color="light" light className="navBar">
         <NavbarBrand href="/"><h1>TO-DO App</h1></NavbarBrand>
         <Nav className="mr-0" navbar>
           <NavbarText >
