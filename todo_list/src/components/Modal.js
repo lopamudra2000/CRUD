942c3f2b7803e7362ab1189c1702d426d5acdff1
Demozy4321 <56303334+Demozy4321@users.noreply.github.com>
2021-06-08 21:29:31 +0530

    Add files via upload

--- todo_list/src/components/Modal.js
+++ todo_list/src/components/Modal.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import "../App.css";
 
-const Modal = props => {
+const Modal = (props) => {
   return ReactDOM.createPortal(
-    <div onClick={props.onDismiss} className="ui dimmer modals visible active">
+    <div
+      onClick={props.onDismiss}
+      className="ui dimmer modals visible active modal-mid-back"
+    >
       <div
-        onClick={e => e.stopPropagation()}
-        className="ui standard modal visible active"
+        id="modal-mid"
+        onClick={(e) => e.stopPropagation()}
+        className="ui standard modal visible active modal-mid"
       >
         <div className="header">{props.title}</div>
         <div className="content">{props.content}</div>
         <div className="actions">{props.actions}</div>
       </div>
     </div>,
-    document.querySelector('#modal')
+    document.querySelector("#modal")
   );
 };
 
